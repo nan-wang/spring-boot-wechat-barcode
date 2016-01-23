762852fedbc0903edf4319b446988f4d685f9498
Nan Wang <Nan.Wang@zalando.de>
2016-01-23 19:16:07 +0100

    complete the first draft.

---
+++ src/main/java/com/ojins/app/controller/WechatController.java
@@ -0,0 +1,101 @@
+package com.ojins.app.controller;
+
+import me.chanjar.weixin.mp.api.*;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import me.chanjar.weixin.common.util.StringUtils;
+import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
+
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by nwang on 23/01/16.
+ */
+@RestController
+public class WechatController {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+    WxMpInMemoryConfigStorage config = new WxMpInMemoryConfigStorage();
+    private WxMpService wxMpService = new WxMpServiceImpl();
+    private WxMpMessageRouter wxMpMessageRouter;
+
+    @PostConstruct
+    public void init() throws Exception {
+        config.setAppId("wx32c7228069d88769"); // 设置微信公众号的appid
+        config.setSecret("24bba82c919a543982c78e77ed4b3863"); // 设置微信公众号的app corpSecret
+        config.setToken("ojinscom"); // 设置微信公众号的token
+        config.setAesKey("aoGcSLZpgW12i46uP8AZ2w6z707nSYmg1YLUsxj2Ee2"); // 设置微信公众号的EncodingAESKey
+
+        wxMpService.setWxMpConfigStorage(config);
+
+        WxMpMessageHandler handler =
+                (wxMpXmlMessage, map, wxMpService1, wxSessionManager) -> WxMpXmlOutMessage
+                        .TEXT()
+                        .content("测试加密消息")
+                        .fromUser(wxMpXmlMessage.getToUserName())
+                        .toUser(wxMpXmlMessage.getFromUserName())
+                        .build();
+
+        wxMpMessageRouter = new WxMpMessageRouter(wxMpService);
+        wxMpMessageRouter
+                .rule()
+                .async(false)
+                .content("哈哈") // 拦截内容为“哈哈”的消息
+                .handler(handler)
+                .end();
+        logger.info("initialized handler.");
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            produces = "text/html;charset=utf-8")
+    public ResponseEntity<String> getResponse(@RequestParam String signature,
+                                              @RequestParam String nonce,
+                                              @RequestParam String timestamp,
+                                              @RequestParam(defaultValue = "") String echostr,
+                                              @RequestParam(defaultValue = "raw") String encrypt_type,
+                                              @RequestParam(defaultValue = "") String msg_signature,
+                                              @RequestPart String request) throws IOException {
+        if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
+            // 消息签名不正确，说明不是公众平台发过来的消息
+            logger.warn("消息签名不正确");
+            return new ResponseEntity<>("非法请求", HttpStatus.OK);
+        }
+
+        if (StringUtils.isNotBlank(echostr)) {
+            // 说明是一个仅仅用来验证的请求，回显echostr
+            logger.info("一个仅仅用来验证的请求");
+            return new ResponseEntity<>("非法请求", HttpStatus.OK);
+        }
+
+        if ("raw".equals(encrypt_type)) {
+            // 明文传输的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(
+                    new ByteArrayInputStream(request.getBytes(StandardCharsets.UTF_8)));
+            WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+            logger.info("明文传输的消息");
+            return new ResponseEntity<>(outMessage.toXml(), HttpStatus.OK);
+        }
+
+        if ("aes".equals(encrypt_type)) {
+            // 是aes加密的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage
+                    .fromEncryptedXml(
+                            new ByteArrayInputStream(request.getBytes(StandardCharsets.UTF_8)),
+                            config, timestamp, nonce, msg_signature);
+            WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+            logger.info("是aes加密的消息");
+            return new ResponseEntity<>(outMessage.toXml(), HttpStatus.OK);
+        }
+
+        logger.warn("不可识别的加密类型");
+        return new ResponseEntity<>("不可识别的加密类型", HttpStatus.OK);
+    }
+}
