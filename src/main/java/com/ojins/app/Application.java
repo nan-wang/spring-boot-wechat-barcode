179bf54fc91527b0d39187bf8195cad5a1b96f39
Nan Wang <Nan.Wang@zalando.de>
2016-01-23 23:07:01 +0100

    fix the broken pom

--- src/main/java/com/ojins/app/Application.java
+++ src/main/java/com/ojins/app/Application.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class Application
-{
+public class Application {
     public static void main( String[] args ) throws Exception
     {
         SpringApplication.run(Application.class, args);
